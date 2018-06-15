6974317c92da056d288642296b14edef346370af
Soham Nakhare <sohamnakhare@gmail.com>
2018-06-15 14:14:13 +0530

    handled update user case

--- definitions/database.js
+++ definitions/database.js
@@ -16,9 +16,20 @@ function watchCollections(db) {
 	const collection = db.collection('users')
 	const changeStream = collection.watch({ fullDocument: 'updateLookup' });
 	changeStream.on('change', function (change) {
-		if(change.operationType === 'insert') {
+		if (change.operationType === 'insert') {
 			const user = change.fullDocument;
 			F.global.users.push(user);
+		}
+
+		if (change.operationType === 'update') {
+			const user = change.fullDocument;
+			var i = F.global.users.findIndex((userObj) => {
+				return user.email === userObj.email
+			});
+			if (i != -1) {
+				const oldUser = Object.assign({}, F.global.users[i]);
+				F.global.users[i] = Object.assign({}, oldUser, user);
+			}
 		}
 	});
 }
