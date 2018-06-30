3f933eb94a9a6ab5731b5d0de205242758611ca7
Soham Nakhare <sohamnakhare@gmail.com>
2018-06-30 21:18:54 +0530

    login with facebook

--- controllers/oAuth2Controller.js
+++ controllers/oAuth2Controller.js
@@ -1,9 +1,14 @@
 const OAUTH_CLIENTID = '178466460871-h7rd1j9fqi3kj31v2uu2964ve6341d00.apps.googleusercontent.com';
 const OAUTH_CLIENT_SECRET = 'QgBE4Uplms4-zOGVLh1vMKDi';
 
+const FACEBOOK_APPID = '381736985661759';
+const FACEBOOK_SECRET = 'af02a082d64ac33476fb16f7f1c93c77';
+
 exports.install = function() { 
     F.route('/login/google/', oauth_login, ['unauthorize']);
     F.route('/login/google/callback/', oauth_login_callback, ['unauthorize']);
+    F.route('/login/facebook/', oauth_login_facebook, ['unauthorize']);
+    F.route('/login/facebook/callback/', oauth_login_facebook_callback, ['unauthorize']);
 };
 
 function oauth_login() {
@@ -27,6 +32,31 @@ function oauth_login_callback() {
             var email = profile.emails[0].value;
             OPERATION('admin.notify', { type: 'admin.login', message: email});
             setCookies(profile, email, self);
+    });
+}
+
+function oauth_login_facebook() {
+    var self = this;
+    var type = self.req.path[1];
+
+    self.host(F.config.base_path_assets+
+        '/login/facebook/callback/')
+
+    MODULE('oauth2').redirect(type, FACEBOOK_APPID,
+        self.host('/messenger/login/facebook/callback/'), self);
+}
+
+function oauth_login_facebook_callback() {
+    var self = this;
+    var type = self.req.path[1];
+    var url = self.host('/messenger/login/' + type + '/callback/');
+
+    MODULE('oauth2').callback(type, FACEBOOK_APPID, FACEBOOK_SECRET, url, self,
+        function(err, profile, access_token) {
+            // var email = profile.emails[0].value;
+            // OPERATION('admin.notify', { type: 'admin.login', message: email});
+            // setCookies(profile, email, self);
+            console.log('profile: ', profile);
     });
 }
 
